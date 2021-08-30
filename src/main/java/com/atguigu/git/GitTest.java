87921db4993e2cc3c1e0b2294c0038e6fa57f828
yangyi <yangyi0907@mail.ustc.edu.cn>
2021-08-30 16:22:37 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -2,11 +2,11 @@ package com.atguigu.git;
 
 /**
  * @author YANGYI
- * @create 2021/8/30
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
